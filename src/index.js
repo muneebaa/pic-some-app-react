119ba6719f3b40d2493e27cc2a7af3383875b992
Muneeba Dilawaze <muneeba.dilawaze@gmail.com>
2022-01-29 20:25:28 +0500

    added hash-router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { ContextProvider } from "./Context"
 
 ReactDOM.render(
   <ContextProvider>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </ContextProvider>,
   document.getElementById("root")
 )
